8757115db83aec515996e5bac68cebbe0a6862d0
Alona Kuznietsova <acvetochka@gmail.com>
2023-12-07 13:20:35 +0100

    add generic tasks

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,13 +11,16 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+type Top = Pick<AllType, "name" | "color">;
+type Bottom = Pick<AllType, "position" | "weight">;
+
+function compare<T extends Top, U extends Bottom>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
